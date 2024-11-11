258152d3973a73329a49361a5c468f65f6521e17
Carol Araya <carayacn@gmail.com>
2024-11-11 00:19:28 -0600

    Interfaz Menu y Jugar

---
+++ Futoshiki/src/futoshiki/Futoshiki.java
@@ -0,0 +1,16 @@
+package futoshiki;
+import Vista.Jugar;
+
+public class Futoshiki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Jugar jugar = new Jugar();
+        jugar.setVisible(true);
+        
+    }
+    
+}
